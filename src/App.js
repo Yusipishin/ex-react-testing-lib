9c41900177fef5e1ea0929673cd139dc1b915973
Alexey <alexey.yus@mail.ru>
2024-03-28 08:54:56 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button>Click me!</button>
+      <input type="text" placeholder="input vfalue..." />
+    </div>
+  );
+}
+
+export default App;
