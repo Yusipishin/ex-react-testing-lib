e647b8ff92e72e1ce90957570940e34aed07d9c1
Alexey <alexey.yus@mail.ru>
2024-03-28 09:22:43 +0300

    findBy & getBy & queryBy & async example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import {useEffect, useState} from "react";
+
 function App() {
-  return (
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false)
+
+    const onClick = () => setToggle(prev => !prev)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData({})
+        }, 100)
+    }, []);
+
+    return (
     <div>
-      <h1>Hello World!</h1>
-      <button>Click me!</button>
-      <input type="text" placeholder="input vfalue..." />
+        {toggle && <div data-testid="toggle-elem" >toggle</div>}
+        {data && <div style={{color: 'red'}}>DATA</div>}
+        <h1>Hello World!</h1>
+        <button data-testid="toggle-btn" onClick={onClick}>Click me!</button>
+        <input type="text" placeholder="input vfalue..."/>
     </div>
-  );
+)
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+describe('APP TEST', () => {
+    test('renders learn react link', () => {
+      render(<App />);
 
-  const helloWorldElement = screen.getByText(/hello world!/i)
-  const btn = screen.getByRole('button')
-  const input = screen.getByPlaceholderText(/input value.../i)
+      const helloWorldElement = screen.getByText(/hello world!/i)
+      const btn = screen.getByRole('button')
+      const input = screen.getByPlaceholderText(/input vfalue.../i)
 
-  expect(helloWorldElement).toBeInTheDocument()
-  expect(btn).toBeInTheDocument()
-  expect(input).toMatchSnapshot()
+      expect(helloWorldElement).toBeInTheDocument()
+      expect(btn).toBeInTheDocument()
+      expect(input).toMatchSnapshot()
+  });
+
+    // find -- для асинхронного кода
+    // get -- 100% получение элемента
+    // query -- доказать отсутствие
+
+    test('renders learn react link', async() => {
+      render(<App />);
+
+      // const helloWorldElement = screen.queryByText(/hello142/i)
+      // expect(helloWorldElement).toBeNull()
+
+      const helloWorldElement = await screen.findByText(/data/i)
+      expect(helloWorldElement).toBeInTheDocument()
+      expect(helloWorldElement).toHaveStyle({color: 'red'})
+
+  });
 });
