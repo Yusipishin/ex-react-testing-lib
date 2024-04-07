e2260842bb54f1b4e52b701de741240441cbee85
Alexey <alexey.yus@mail.ru>
2024-04-07 22:15:48 +0300

    renderTestApp & renderWithRedux

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -3,16 +3,18 @@ import {render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import {Provider} from "react-redux";
 import {createReduxStore} from "../../store/store";
+import {renderWithRedux} from "../../tests/helpers/renderWithRedux";
+import {renderTestApp} from "../../tests/helpers/renderTestApp";
 
 describe('Counter test', () => {
     test('Test ', async () => {
-        const {getByTestId} = render(
-            <Provider store={createReduxStore({
-                counter: {value: 10}
-            })}>
-                <Counter/>
-            </Provider>
-        )
+        const {getByTestId} =
+            renderTestApp(null, {
+                route: "/",
+                initialState: {
+                    counter: { value: 10 }
+                }
+            })
         //getByTestId будет работать в области <Counter/>
         const incrementBtn = getByTestId('incr-btn')
         expect(getByTestId('value-title')).toHaveTextContent("10")
